9ed57ccaec5312fbc9ea7ac2d0cc15528eff2cda
juanfranciscofernandezherreros@bitbucket.org <juanfranciscofernandezherreros@bitbucket.org>
2021-05-03 00:28:10 +0200

    feat : doc

---
+++ src/main/java/com/fernandez/basketball/utils/DocumenUtils.java
@@ -0,0 +1,51 @@
+package com.fernandez.basketball.utils;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+public class DocumenUtils {
+
+    /**
+     * Con este método devuelvo un objeto de la clase Document con el contenido del
+     * HTML de la web que me permitirá parsearlo con los métodos de la librelia JSoup
+     * @param url
+     * @return Documento con el HTML
+     */
+    public static Document getHtmlDocument(String url) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).get();
+        } catch (IOException ex) {
+            System.out.println("Excepción al obtener el HTML de la página" + ex.getMessage());
+        }
+        return doc;
+    }
+
+    /**
+     * Con esta método compruebo el Status code de la respuesta que recibo al hacer la petición
+     * EJM:
+     * 		200 OK			300 Multiple Choices
+     * 		301 Moved Permanently	305 Use Proxy
+     * 		400 Bad Request		403 Forbidden
+     * 		404 Not Found		500 Internal Server Error
+     * 		502 Bad Gateway		503 Service Unavailable
+     * @param url
+     * @return Status Code
+     */
+    public static int getStatusConnectionCode(String url) {
+
+        Connection.Response response = null;
+
+        try {
+            response = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).ignoreHttpErrors(true).execute();
+        } catch (IOException ex) {
+            System.out.println("Excepción al obtener el Status Code: " + ex.getMessage());
+        }
+        return response.statusCode();
+    }
+
+
+}
