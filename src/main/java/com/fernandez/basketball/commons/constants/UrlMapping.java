41b94bf651e85bb98b5fab3c44df88024c68db07
juanfranciscofernandezherreros@bitbucket.org <juanfranciscofernandezherreros@bitbucket.org>
2021-05-02 10:20:53 +0200

    feat : upload file on folders

--- src/main/java/com/fernandez/basketball/commons/constants/UrlMapping.java
+++ src/main/java/com/fernandez/basketball/commons/constants/UrlMapping.java
@@ -5,7 +5,6 @@ public final class UrlMapping {
     public static final String V1 = "/v1";
     public static final String PUBLIC = "/public";
     public static final String MAIN_RESOURCES = "src/main/resources";
-    public static final String PLAYBYPLAY_STR = "PLAYBYPLAY";
     public static final String PLAYBYPLAY = "/playByPlay";
     public static final String EVOLUTION = "/evolution";
     public static final String HEADER = "/header";
--- src/main/java/com/fernandez/basketball/file/upload/controller/UploadFileController.java
+++ src/main/java/com/fernandez/basketball/file/upload/controller/UploadFileController.java
@@ -1,6 +1,5 @@
 package com.fernandez.basketball.file.upload.controller;
 
-import com.fernandez.basketball.commons.constants.UrlMapping;
 import com.fernandez.basketball.file.upload.service.FileStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -13,10 +12,10 @@ public class UploadFileController {
     @Autowired
     private FileStorage fileStorage;
 
-    @RequestMapping(value = "/file/upload", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody String myService(@RequestParam("file") MultipartFile file) throws Exception {
+    @RequestMapping(value = "/file/upload{}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody String myService(@RequestParam("file") MultipartFile file , @PathVariable String myEnum) throws Exception {
         try {
-            fileStorage.store(file,"playByPlay");
+            fileStorage.store(file,myEnum);
             return "File uploaded successfully! -> filename = " + file.getOriginalFilename();
         } catch (Exception e) {
             return "Fail! -> uploaded filename: " + file.getOriginalFilename();
