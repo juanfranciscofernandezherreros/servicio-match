1b07542e5ea78dff88c1e8fbd77092709971a17b
juanfranciscofernandezherreros@bitbucket.org <juanfranciscofernandezherreros@bitbucket.org>
2021-06-13 22:16:54 +0200

    avoid teams duplicated

--- src/main/java/com/fernandez/basketball/euroleague/teams/repository/TeamsRepository.java
+++ src/main/java/com/fernandez/basketball/euroleague/teams/repository/TeamsRepository.java
@@ -6,8 +6,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TeamsRepository extends MongoRepository<TeamsDTO,String > {
 
     Page<TeamsDTO> findAllTeamsBySeasson(String seasson, Pageable pageable);
- }
+
+    List<TeamsDTO> findAllTeamsBySeasson(String seasson);
+
+}
